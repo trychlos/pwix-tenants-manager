853163e7aacfdca8e77744738d908ee9ca63d9e6
Pierre Wieser <p.wieser@trychlos.org>
2024-10-04 15:11:18 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:tenants-manager',
-    version: '1.3.0',
+    version: '1.3.1-rc',
     summary: 'A Meteor package to manage tenants with validity periods',
     git: 'https://github.com/trychlos/pwix-tenants-manager',
     documentation: 'README.md'
